a4ec8863e2e572ba0bc275c78f08685b86091c9b
Carolina-Vacaflor <89324013+Carolina-Vacaflor@users.noreply.github.com>
2023-01-23 15:59:19 -0400

    fix comments 1.1

--- Token/main.py
+++ Token/main.py
@@ -16,8 +16,8 @@ from dotenv import load_dotenv
 
 app = Flask(__name__)
 
-app.register_blueprint(routes_auth, url_prefix="/api")
+app.register_blueprint(routes_auth, url_prefix = "/api")
 
 if __name__ == '__main__':
     load_dotenv()
-    app.run(debug=True, port="4000", host="0.0.0.0")
+    app.run(debug = True, port = "4000", host = "0.0.0.0")
