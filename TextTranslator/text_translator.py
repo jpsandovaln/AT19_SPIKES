bcf781c90380eddf8d8e159e5dd76e67153f2423
Carolina-Vacaflor <89324013+Carolina-Vacaflor@users.noreply.github.com>
2023-01-23 16:04:02 -0400

    Update text_translator.py

--- TextTranslator/text_translator.py
+++ TextTranslator/text_translator.py
@@ -1,5 +1,18 @@
+#
+# @text_translator.py Copyright (c) 2023 Jalasoft.
+# 2643 Av Melchor Perez de Olguin, Colquiri Sud, Cochabamba, Bolivia.
+# All rights reserved.
+#
+# This software is the confidential and proprietary information of
+# Jalasoft, ("Confidential Information"). You shall not
+# disclose such Confidential Information and shall use it only in
+# accordance with the terms of the license agreement you entered into
+# with Jalasoft.
+#
+
 import googletrans
 from googletrans import Translator
+
 
 print(googletrans.__version__)
 print(googletrans.LANGUAGES)
